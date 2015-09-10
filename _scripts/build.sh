023e55d902f74d5804b32ca8b7dccf4aca6e1efc
Peter Solymos <psolymos@gmail.com>
2015-09-09 18:17:29 -0600

    build is done

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -1,4 +1,4 @@
-
+## clear old stuff
 rm ~/repos/abmispecies/_data/*.yml -f
 rm ~/repos/abmispecies/pages/species/mammals/* -f
 rm ~/repos/abmispecies/pages/species/vplants/* -f
@@ -6,7 +6,6 @@ rm ~/repos/abmispecies/pages/species/mosses/* -f
 rm ~/repos/abmispecies/pages/species/lichens/* -f
 #rm ~/repos/abmispecies/pages/species/birds/* -f
 rm ~/repos/abmispecies/pages/species/mites/* -f
-
 rmdir ~/repos/abmispecies/pages/species/mammals
 rmdir ~/repos/abmispecies/pages/species/vplants
 rmdir ~/repos/abmispecies/pages/species/mosses
@@ -14,26 +13,17 @@ rmdir ~/repos/abmispecies/pages/species/lichens
 #rmdir ~/repos/abmispecies/pages/species/birds
 rmdir ~/repos/abmispecies/pages/species/mites
 
-
+## status update
 cd ~/repos/abmispecies
 git pull
 #git checkout --track -b origin/v2015
 git checkout v2015
+
+## run R script
 cd ~/repos/abmispecies/_scripts
 Rscript --vanilla website_script.R
 
+## run Jekyll
+cd ~/repos/abmispecies/
+jekyll build --destination /var/www/html
 
-unzip abmispecies.zip -d /home/peter/www/species.abmi.ca
-cd /home/peter/www/species.abmi.ca
-rm _site/site.tar.gz
-jekyll build
-tar -zcf /home/peter/Dropbox/www/sitebuild.tar.gz -C /home/peter/www/species.abmi.ca/_site .
-
-rm ~/bam/*
-module load application/git/1.7.10.1
-cd ~/repos/bamanalytics/
-git pull
-cd ~/repos/bragging/
-git pull
-cd ~/bam/
-cp ~/repos/bamanalytics/wg/* ~/bam/
