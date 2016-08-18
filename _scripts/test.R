8e30a825558a7d3a93f4d0c04555a11b361de3bc
Peter Solymos <psolymos@gmail.com>
2016-08-18 13:13:50 -0600

    x

--- _scripts/test.R
+++ _scripts/test.R
@@ -1,12 +1,12 @@
-ROOT <- "~/repos/abmispecies/_data"
-OUT <- if (interactive())
-    "e:/peter/sppweb-pages/species" else "~/repos/abmispecies/pages/testspecies"
-OUT2 <- if (interactive())
-    "e:/peter/sppweb-pages/species" else "~/repos/abmispecies/_data"
+DIR1 <- "~/repos/abmispecies/_data"
+DIR2 <- "~/repos/abmispecies/pages/testspecies"
 
-spptab <- read.csv(file.path(ROOT, "birds2.csv"))
+spptab <- read.csv(file.path(DIR1, "birds2.csv"))
 rownames(spptab) <- spptab$sppid
 spptab <- spptab[order(spptab$family, spptab$species),]
+
+cat("read tab\n")
+warnings()
 
 ## YAML front matter variables
 yaml_directives <-
@@ -15,7 +15,6 @@ Prev=NA, Next=NA)
 {
         scinam <- as.character(spptab[spplabel, "scinam"])
         comnam <- as.character(spptab[spplabel, "species"])
-        nnstatus <- ifelse(spptab[spplabel, "NN"], "nonnative", "native")
         title <- title_format
         title <- sub("scinam", scinam, title)
         title <- sub("comnam", comnam, title)
@@ -44,8 +43,11 @@ for (i in 1:nrow(spptab)) {
     Next <- if (i == nrow(spptab))
         NA else as.character(spptab$species[i+1])
     yaml <- yaml_directives(spp, Prev=Prev, Next=Next)
-    writeLines(yaml, file.path(OUT, "birds", paste0(spp, ".html")))
+    writeLines(yaml, file.path(DIR2, "birds", paste0(spp, ".html")))
 }
+
+cat("write headers\n")
+warnings()
 
 ## generating index
 spptab0 <- spptab
@@ -69,5 +71,8 @@ for (lead_var in lead_vars) {
         res[[a]] <- c(lev1, lev2)
     }
     res <- unname(unlist(res))
-    writeLines(res, file.path(OUT2, paste0("birds2_", substr(lead_var, 1, 3), ".yml")))
+    writeLines(res, file.path(DIR1, paste0("birds2_", substr(lead_var, 1, 3), ".yml")))
 }
+
+cat("write toc\n")
+warnings()
