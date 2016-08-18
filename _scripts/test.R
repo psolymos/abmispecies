1fa013bedac30d9de051c60bdc7377735e1eddba
Peter Solymos <psolymos@gmail.com>
2016-08-18 12:33:41 -0600

    test suite

---
+++ _scripts/test.R
@@ -0,0 +1,72 @@
+ROOT <- "~/repos/abmispecies/_data"
+OUT <- if (interactive())
+    "e:/peter/sppweb-pages/species" else "~/repos/abmispecies/pages/testspecies"
+OUT2 <- if (interactive())
+    "e:/peter/sppweb-pages/species" else "~/repos/abmispecies/_data"
+
+spptab <- read.csv(file.path(ROOT, "birds2.csv"))
+rownames(spptab) <- spptab$sppid
+spptab <- spptab[order(spptab$family, spptab$species),]
+
+## YAML front matter variables
+yaml_directives <-
+function(spplabel, title_format="comnam (<em>scinam</em>)", layout="testspecies",
+Prev=NA, Next=NA)
+{
+        scinam <- as.character(spptab[spplabel, "scinam"])
+        comnam <- as.character(spptab[spplabel, "species"])
+        nnstatus <- ifelse(spptab[spplabel, "NN"], "nonnative", "native")
+        title <- title_format
+        title <- sub("scinam", scinam, title)
+        title <- sub("comnam", comnam, title)
+        out <- c("---",
+            paste0("layout: ", layout),
+            paste0("title: ", title),
+            paste0("spplabel: ", spplabel),
+            paste0("description: \"ABMI species summary for ", scinam, "\""),
+            #"sidebar: true",
+            paste0("taxon: ", "birds"),
+            paste0("taxonname: ", "Birds"),
+            paste0("singing: ", ifelse(spptab[spplabel, "singing"], "true", "false")),
+            paste0("previous: ", ifelse(is.na(Prev), "", Prev)),
+            paste0("next: ", ifelse(is.na(Next), "", Next)),
+            paste0("family: ", as.character(spptab[spplabel, "family"])),
+            "---")
+        out
+}
+yaml_directives(rownames(spptab)[1])
+
+for (i in 1:nrow(spptab)) {
+    spp <- rownames(spptab)[i]
+    Prev <- if (i == 1)
+        NA else as.character(spptab$species[i-1])
+    Next <- if (i == nrow(spptab))
+        NA else as.character(spptab$species[i+1])
+    yaml <- yaml_directives(spp, Prev=Prev, Next=Next)
+    writeLines(yaml, file.path(OUT, taxon, paste0(spp, ".html")))
+}
+
+## generating index
+spptab0 <- spptab
+lead_vars <- c("comnam", "scinam")
+for (lead_var in lead_vars) {
+    spptab <- spptab0
+    tmp <- spptab[order(spptab[[lead_var]]),]
+    lead <- substr(as.character(tmp[[lead_var]]), 1, 1)
+    ulead <- unique(lead)
+    sppfun <- function(i, tmp) {
+        c(paste0("    - scinam: ", tmp$scinam[i]),
+        paste0("      comnam: ", tmp$comnam[i]),
+        paste0("      label: ", rownames(tmp)[i]),
+        paste0("      model: ", ifelse(tmp$FULL[i], "true", "false")))
+    }
+    res <- list()
+    for (a in ulead) {
+        lev1 <- c("-", paste0("  letter: ", a), "  species:")
+        zz <- tmp[lead == a,,drop=FALSE]
+        lev2 <- lapply(1:nrow(zz), sppfun, tmp=zz)
+        res[[a]] <- c(lev1, lev2)
+    }
+    res <- unname(unlist(res))
+    writeLines(res, file.path(OUT2, paste0(taxon, "2_", substr(lead_var, 1, 3), ".yml")))
+}
