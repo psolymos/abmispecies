ba4bea9ef6ae01812a26275bc6daba66b58f49ee
Peter Solymos <psolymos@gmail.com>
2018-10-19 15:29:44 -0600

    home set

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 ## run like:
-## sudo bash ~/repos/abmispecies/_scripts/build.sh
+## sudo bash /home/solymos/repos/abmispecies/_scripts/build.sh
 
 ## clear old stuff
 echo 'Cleaning up old stuff'
-rm -f ~/repos/abmispecies/_data/*.yml
-rm -r -f ~/repos/abmispecies/pages/species/mammals
-rm -r -f ~/repos/abmispecies/pages/species/vplants
-rm -r -f ~/repos/abmispecies/pages/species/mosses
-rm -r -f ~/repos/abmispecies/pages/species/lichens
-rm -r -f ~/repos/abmispecies/pages/species/birds
-rm -r -f ~/repos/abmispecies/pages/species/mites
-#rm -r -f ~/repos/abmispecies/pages/testspecies/birds
+rm -f /home/solymos/repos/abmispecies/_data/*.yml
+rm -r -f /home/solymos/repos/abmispecies/pages/species/mammals
+rm -r -f /home/solymos/repos/abmispecies/pages/species/vplants
+rm -r -f /home/solymos/repos/abmispecies/pages/species/mosses
+rm -r -f /home/solymos/repos/abmispecies/pages/species/lichens
+rm -r -f /home/solymos/repos/abmispecies/pages/species/birds
+rm -r -f /home/solymos/repos/abmispecies/pages/species/mites
+#rm -r -f /home/solymos/repos/abmispecies/pages/testspecies/birds
 
 ## status update
-cd ~/repos/abmispecies
+cd /home/solymos/repos/abmispecies
 echo 'Updating git repo'
 #git checkout --track -b origin/v2015
 #git checkout v2015
@@ -25,13 +25,13 @@ git pull
 
 ## run R script
 echo 'Running R script'
-cd ~/repos/abmispecies/_scripts
+cd /home/solymos/repos/abmispecies/_scripts
 Rscript --vanilla website_script.R
 #Rscript --vanilla test.R
 
 ## run Jekyll
 echo 'Running jekyll'
-cd ~/repos/abmispecies/
+cd /home/solymos/repos/abmispecies/
 rm -r -f /var/www/html/development
 mkdir /var/www/html/development
 jekyll build --destination /var/www/html/development
@@ -40,14 +40,14 @@ jekyll build --destination /var/www/html/development
 echo 'Packing up site content'
 cd /var/www/html/development
 zip -r -q sppweb.zip .
-mv sppweb.zip ~/repos/abmispecies/_site/
+mv sppweb.zip /home/solymos/repos/abmispecies/_site/
 
 ## return to home folder
-#cd ~
+#cd /home/solymos
 
 ## redirect
 echo 'Redirect overwrite'
-cp ~/repos/abmispecies/_scripts/index.html /var/www/html/index.html
-cp ~/repos/abmispecies/_scripts/robots.txt /var/www/html/robots.txt
+cp /home/solymos/repos/abmispecies/_scripts/index.html /var/www/html/index.html
+cp /home/solymos/repos/abmispecies/_scripts/robots.txt /var/www/html/robots.txt
 
 echo 'Done'
